9ecae1020cc824a6abd9898328a974a48feee5ea
NotAPenguin <michiel.vandeginste@gmail.com>
2020-03-12 17:28:46 +0100

    add dockspace

--- src/editor/editor_system.cpp
+++ src/editor/editor_system.cpp
@@ -4,6 +4,23 @@
 #include <imgui/imgui.h>
 
 void EditorSystem::update(saturn::FrameContext& ctx) {
+    ImGuiWindowFlags flags =
+        ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+    ImGuiViewport* viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(viewport->Pos);
+    ImGui::SetNextWindowSize(viewport->Size);
+    ImGui::SetNextWindowViewport(viewport->ID);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+    flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
+             ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+    flags |=
+        ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+    ImGui::Begin("MainEditorSpace", nullptr, flags);
+    ImGui::PopStyleVar(3);
+
+    // DockSpace
     ImGuiIO& io = ImGui::GetIO();
     if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
         ImGuiID dockspace_id = ImGui::GetID("MainDockSpace");
@@ -16,6 +33,8 @@ void EditorSystem::update(saturn::FrameContext& ctx) {
         auto img = ctx.render_info.present_manager->get_attachment(ctx.render_info, "color1");
         ImGui::Image(img.get_imgui_tex_id(), ImVec2(img.get_width(), img.get_height()));
     }
+
+    ImGui::End();
 
     ImGui::End();
 }
